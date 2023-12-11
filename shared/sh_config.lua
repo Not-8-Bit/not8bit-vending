53997d0951faab7e3a5832e5537e1a0e39bd959e
lllHolidaylll <xholiday81x@gmail.com>
2023-12-11 14:00:15 -0600

    save config changes

---
+++ shared/sh_config.lua
@@ -0,0 +1,73 @@
+Config = {}
+
+Config.VendingItems = {
+    prop_vend_soda_01 = {
+        { name = 'water_bottle', price = 7 , amount = 50 , info = { uses = 4 } , type = 'item' , slot = 1 } ,
+        { name = 'ecola'       , price = 6 , amount = 50 , info = { uses = 2 } , type = 'item' , slot = 2 } ,
+    } ,
+    prop_vend_water_01 = {
+        { name = 'water_bottle' , price = 7 , amount = 50 , info = { uses = 4 } , type = 'item' , slot = 1 }
+    } ,
+    prop_vend_coffe_01 = {
+        { name = 'coffee'     , price = 15 , amount = 50 , info = { uses = 2 } , type = 'item' , slot = 1 } ,
+        { name = 'icedcoffee' , price = 20 , amount = 50 , info = { uses = 2 } , type = 'item' , slot = 2 } ,
+    } ,
+    prop_vend_soda_02 = {
+        { name = 'water_bottle' , price = 7 , amount = 50 , info = { uses = 4 } , type = 'item' , slot = 1 } ,
+        { name = 'sprunk'       , price = 6 , amount = 50 , info = { uses = 2 } , type = 'item' , slot = 2 } ,
+    } ,
+    p_ld_coffee_vend_01 = {
+        { name = 'capucciano' , price = 25 , amount = 50 , info = { uses = 2 } , type = 'item' , slot = 1 } ,
+        { name = 'coffee'     , price = 15 , amount = 50 , info = { uses = 2 } , type = 'item' , slot = 2 } ,
+    } ,
+    prop_vend_fridge01 = {
+        { name = 'water_bottle' , price = 6 , amount = 50 , info = { uses = 3 } , type = 'item' , slot = 1 } ,
+        { name = 'ecola'        , price = 6 , amount = 50 , info = { uses = 2 } , type = 'item' , slot = 2 } ,
+    } ,
+    prop_vend_snak_01 = {
+        { name = 'pandqs'    , price = 7 , amount = 50 , info = { uses = 2 } , type = 'item' , slot = 1 } ,
+        { name = 'egochaser' , price = 7 , amount = 50 , info = { uses = 2 } , type = 'item' , slot = 3 } ,
+    } ,
+    prop_vend_snak_01_tu = {
+        { name = 'pandqs'       , price = 7 , amount = 50   , info = { uses = 2 } , type = 'item' , slot = 1 } ,
+        { name = 'earthquakes'  , price = 6 , amount = 50   , info = { uses = 2 } , type = 'item' , slot = 2 } ,
+    } ,
+    prop_hotdogstand_01 = {
+        { name = 'hotdog' , price = 5 , amount = 50 , info = { uses = 4 } , type = 'item' , slot = 1 } ,
+    } ,
+    prop_burgerstand_01 = {
+        { name = 'cheeseburger' , price = 10 , amount = 50 , info = { uses = 2 } , type = 'item' , slot = 1 } ,
+    } ,
+    v_ret_247_donuts = {
+        { name = 'creameddonut'    , price = 5 , amount = 50 , info = { uses = 2 } , type = 'item' , slot = 1 } ,
+        { name = 'strawberrydonut' , price = 5 , amount = 50 , info = { uses = 2 } , type = 'item' , slot = 2 } ,
+    } ,
+    prop_juice_dispenser = {
+        { name = 'slushy'        , price =  7 , amount = 50 , info = { uses = 2 } , type = 'item' , slot = 1 } ,
+        { name = 'cherrysludgee' , price = 15 , amount = 50 , info = { uses = 2 } , type = 'item' , slot = 2 } ,
+    } ,
+    prop_vend_fags_01 = {
+        { name = 'cigarettebox' , price = 20 , amount = 50 , info = { uses = 20 } , type = 'item' , slot = 1 } ,
+    } ,
+    prop_pool_rack_01 = {
+        { name = 'weapon_poolcue' , price = 25 , amount = 10 , info = { } , type = 'item' , slot = 1 } ,
+    } ,
+    prop_vend_condom_01 = {
+        { name = 'condom'   , price = 5 , amount = 100 , info = { } , type = 'item' , slot = 1 } ,
+        { name = 'xscondom' , price = 5 , amount = 100 , info = { } , type = 'item' , slot = 2 } ,
+        { name = 'xlcondom' , price = 5 , amount = 100 , info = { } , type = 'item' , slot = 3 } ,
+    } ,
+    prop_gumball_01 = {
+        { name = 'gumball' , price = 1 , amount = 400 , info = { } , type = 'item' , slot = 1 } ,
+    } ,
+    prop_gumball_02 = {
+        { name = 'gumball' , price = 1 , amount = 400 , info = { } , type = 'item' , slot = 1 } ,
+    } ,
+    prop_gumball_03 = {
+        { name = 'gumball' , price = 1 , amount = 400 , info = { } , type = 'item' , slot = 1 } ,
+    } ,
+     bzzz_prop_beer_machine = {
+        { name = 'lsoriginalbeer' , price = 1 , amount = 400 , info = { uses = 5 } , type = 'item' , slot = 1 } ,
+        { name = 'lsapplebeer'    , price = 1 , amount = 400 , info = { uses = 5 } , type = 'item' , slot = 2 } ,
+     } ,
+}
